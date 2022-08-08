d2744358cdc3dd53a4bae42602e6205572c564e7
Mason Stuart <mason.stuart05@outlook.com>
2022-08-08 00:58:52 -0700

    Humanradableformat

--- .vscode/humanreadableformat.cpp
+++ .vscode/humanreadableformat.cpp
@@ -11,35 +11,5 @@ std::string format_duration(int seconds) {
   
   const unsigned int yearSecs = 31536000, daySecs = 86400, hourSecs = 3600, minSecs = 60;
   
-  if (seconds / yearSecs >= 1) {
-    time << floor(seconds / yearSecs) << " years, ";
-    seconds -= floor(seconds / yearSecs);
-  }
-  
-  
-  
-  if (seconds / daySecs >= 1) {
-    time += floor(seconds / daySecs) << " days, ";
-    seconds -= floor(seconds / daySecs);
-  }
-  
-  
-  
-  if (seconds / hourSecs >= 1) {
-    time += floor(seconds / hourSecs) << " hours, ";
-    seconds -= floor(seconds / hourSecs);
-  }
-  
-  
-  
-  if (seconds / minSecs >= 1) {
-    time += floor(seconds / minSecs) << " minutes";
-    seconds -= floor(seconds / minSecs);
-  }
-  
-  if (seconds >= 1) {
-    time += " and " + seconds << " seconds";
-  }
-  
-  return time;
+  tYears += (floor(seconds / yearSecs >= 1) ? (floor(seconds / yearSecs)) : 0);
 }
